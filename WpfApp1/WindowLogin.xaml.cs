4482c3774998dbac5002e2a57fe8ec7c7d7f51af
saman333 <samanraut.sr@gmail.com>
2023-07-27 14:28:38 +0545

    feat: use border and list box in wpf

--- WpfApp1/WindowLogin.xaml.cs
+++ WpfApp1/WindowLogin.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,17 +12,52 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for WindowLogin.xaml
-    /// </summary>
+
     public partial class WindowLogin : Window
     {
         public WindowLogin()
         {
             InitializeComponent();
+        }
+        void submitDetails(object sender, RoutedEventArgs e)
+        {
+            string username = name.Text;
+            string useremail = email.Text;
+            string usercountry = country.Text;
+            //For gender
+            string gender = "None";
+            if (male.IsChecked == true)
+                gender = "Male";
+            else if (female.IsChecked == true)
+                gender = "Female";
+            else if (other.IsChecked == true)
+                gender = "Other";
+            // For Hobbies
+            string? hobby1 = null;
+            string? hobby2 = null;
+            string? hobby3 = null;
+            if (football.IsChecked == true)
+                hobby1 = "Football";
+            if (cricket.IsChecked == true)
+                hobby2 = "Cricket";
+            if (formula1.IsChecked == true)
+                hobby3 = "Formula1";
+            List<string> listBoxItems = new List<string>
+            {
+                $"Name: {username}",
+                $"Email: {useremail}",
+                $"Country: {usercountry}",
+                $"Gender: {gender}",
+                $"Hobbies: {hobby1} {hobby2} {hobby3}",
+
+            };
+            // Storing mylist to ListBox
+            myList.ItemsSource = listBoxItems;
+
         }
     }
 }
