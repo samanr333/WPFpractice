98fdf8638dc79c552ead025f4acedd27863efa1d
saman333 <samanraut.sr@gmail.com>
2023-08-24 13:31:35 +0545

    feat: add validation for null or empty name field

--- Validation/Model/StudentModel.cs
+++ Validation/Model/StudentModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,12 @@ namespace Validation.Model
 {
     public class StudentModel
     {
+        
+
         public string Name { get; set; }
-        public string Address { get; set; }
         public int RollNo { get; set; }
         public string Email { get; set; }
-
+        public string Password { get; set; }
+        
     }
 }
--- Validation/ViewModel/StudentViewModel.cs
+++ Validation/ViewModel/StudentViewModel.cs
@@ -1,33 +1,60 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
 using Validation.Command;
 using Validation.Model;
 
 namespace Validation.ViewModel
 {
-    public class StudentViewModel
+    public class StudentViewModel : IDataErrorInfo
     {
         public string? Name { get; set; }
-        public string? Address { get; set; }
         public int? RollNo { get; set; }
         public string? Email { get; set; }
+        public string? Password { get; set; }
         public RelayCommand relaycommand { get; set; }
         public ObservableCollection<StudentModel> students { get; set; }
         public StudentViewModel()
         {
-            students = new ObservableCollection<StudentModel>
-            {
-                new StudentModel { Name = "Bibek" , Address = "Bhaktapur", RollNo = 25, Email = "parajulibbek@gmail.com"},
-                new StudentModel { Name = "Sachin", Address = "Kathmandu", RollNo = 15, Email = "sachinkhatri@gmail.com"},
-
-            };
+            students = new ObservableCollection<StudentModel>();
             relaycommand = new RelayCommand(this);
         }
         public void OnExecute()
         {
-            StudentModel student = new StudentModel { Name = Name, Address = Address, RollNo = (int)RollNo, Email = Email };
+            StudentModel student = new StudentModel { Name = Name, RollNo = (int)RollNo, Email = Email, Password = Password };
             students.Add(student);
+        }
+        public string Error => null;
+        public string this[string propertyName]
+        {
+            get
+            {
+                return GetValidationError(propertyName);
+            }
+        }
+
+        private string GetValidationError(string propertyName)
+        {
+            string error = null;
+            switch (propertyName)
+            {
+                case "Name":
+                    error = ValidateStudentName();
+                    break;
+
+            }
+            return error;
+
+        }
+        private string? ValidateStudentName()
+        {
+            if (String.IsNullOrWhiteSpace(Name))
+            {
+                return "Student name cannot be empty.";
+            }
+            return null;
         }
     }
 }
