9526e83618ccbf1e3db07645d2d1630d2786df42
saman333 <samanraut.sr@gmail.com>
2023-08-24 11:35:19 +0545

    feat: use list view to display data from students view model

---
+++ Validation/Model/StudentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation.Model
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int RollNo { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ Validation/ViewModel/StudentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Validation.Model;
+
+namespace Validation.ViewModel
+{
+    public class StudentViewModel
+    {
+        public List<StudentModel> students { get; set; }
+        public StudentViewModel()
+        {
+            students = new List<StudentModel>
+            {
+                new StudentModel { Name = "Bibek" , Address = "Bhaktapur", RollNo = 25, Email = "parajulibbek@gmail.com"},
+                new StudentModel { Name = "Sachin", Address = "Kathmandu", RollNo = 15, Email = "sachinkhatri@gmail.com"},
+
+            };
+        }
+    }
+}
