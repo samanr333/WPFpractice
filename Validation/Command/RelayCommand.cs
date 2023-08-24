5377d10eaa8dd37466ad78d7d54c5a027c5abfae
saman333 <samanraut.sr@gmail.com>
2023-08-24 12:43:40 +0545

    feat: setup command to add student

---
+++ Validation/Command/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Validation.ViewModel;
+
+namespace Validation.Command
+{
+    public class RelayCommand: ICommand
+    {
+            StudentViewModel AddButtonClick;
+            public RelayCommand(StudentViewModel studentViewModel)
+            {
+                AddButtonClick = studentViewModel;
+            }
+
+            public event EventHandler? CanExecuteChanged;
+
+            public bool CanExecute(object? parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object? parameter)
+            {
+                AddButtonClick.OnExecute();
+            }
+        }
+    }
+
+
--- Validation/ViewModel/StudentViewModel.cs
+++ Validation/ViewModel/StudentViewModel.cs
@@ -1,19 +1,33 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using Validation.Command;
 using Validation.Model;
 
 namespace Validation.ViewModel
 {
     public class StudentViewModel
     {
-        public List<StudentModel> students { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public int? RollNo { get; set; }
+        public string? Email { get; set; }
+        public RelayCommand relaycommand { get; set; }
+        public ObservableCollection<StudentModel> students { get; set; }
         public StudentViewModel()
         {
-            students = new List<StudentModel>
+            students = new ObservableCollection<StudentModel>
             {
                 new StudentModel { Name = "Bibek" , Address = "Bhaktapur", RollNo = 25, Email = "parajulibbek@gmail.com"},
                 new StudentModel { Name = "Sachin", Address = "Kathmandu", RollNo = 15, Email = "sachinkhatri@gmail.com"},
 
             };
+            relaycommand = new RelayCommand(this);
+        }
+        public void OnExecute()
+        {
+            StudentModel student = new StudentModel { Name = Name, Address = Address, RollNo = (int)RollNo, Email = Email };
+            students.Add(student);
         }
     }
 }
