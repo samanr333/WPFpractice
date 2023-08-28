c90ab9a2e3c31d4f98429d88b0d778bc91ffa7c4
saman333 <samanraut.sr@gmail.com>
2023-08-28 13:15:27 +0545

    feat: use drag and drop in list

--- DragandDrop/ViewModel/ToDoViewModel.cs
+++ DragandDrop/ViewModel/ToDoViewModel.cs
@@ -1,21 +1,62 @@
 ﻿using DragandDrop.Model;
 using System.Collections.ObjectModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+using System.Windows.Input;
 
 namespace DragandDrop.ViewModel
 {
     public class ToDoViewModel
     {
         public string? ToDoElement { get; set; }
-        public ObservableCollection<ToDoModel> ToDo { get; set; }
+        public ObservableCollection<ToDoModel> ToDoList{ get; set; }
+        public ObservableCollection<ToDoModel> DoneList { get; set; }
+        private ToDoModel? draggedItem;
         public ToDoViewModel()
         {
-            ToDo = new ObservableCollection<ToDoModel>()
+            ToDoList = new ObservableCollection<ToDoModel>()
             {
                 new ToDoModel{ToDoElement = "Eat"},
                 new ToDoModel{ToDoElement = "Sleep"},
                 new ToDoModel{ToDoElement = "DeadLift"},
                 new ToDoModel{ToDoElement = "Repeat"},
             };
+        }
+
+        public void ToDoListElementMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Store the dragged item
+            draggedItem = (e.OriginalSource as FrameworkElement)?.DataContext as ToDoModel;
+        }
+
+        public void ToDoListView_Drop(object sender, DragEventArgs e)
+        {
+            if (draggedItem != null)
+            {
+                // Remove the item from the source list
+                ToDoList.Remove(draggedItem);
+
+                // Add the item to the target list
+                DoneList.Add(draggedItem);
+
+                // Reset draggedItem
+                draggedItem = null;
+            }
+        }
+
+        public void DoneListView_Drop(object sender, DragEventArgs e)
+        {
+            if (draggedItem != null)
+            {
+                // Remove the item from the source list
+                DoneList.Remove(draggedItem);
+
+                // Add the item to the target list
+                ToDoList.Add(draggedItem);
+
+                // Reset draggedItem
+                draggedItem = null;
+            }
         }
     }
 }
---
+++ DragandDrop/Views/NavPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DragandDrop.Views
+{
+    /// <summary>
+    /// Interaction logic for NavPage.xaml
+    /// </summary>
+    public partial class NavPage : Page
+    {
+        public NavPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
