b1af01ccd51e45f28bb8388bc9218d1c502165dc
saman333 <samanraut.sr@gmail.com>
2023-09-01 14:46:27 +0545

    feat: use textbox custom control in wpf

---
+++ CustomControls/Command/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CustomControls.Command
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> _Execute { get; set; }
+        private Predicate<object> _CanExecute { get; set; }
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return _CanExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _Execute(parameter);
+        }
+    }
+}
---
+++ CustomControls/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CustomControls.Command;
+using System.Windows.Input;
+
+namespace CustomControls.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public MainWindow mainWindow { get; set; }
+        public string TextBox { get; set; }
+        public ICommand DisplayCommand { get; set; }
+        public MainWindowViewModel()
+        {
+            DisplayCommand = new RelayCommand(Display, CanDisplay);
+        }
+        public string ShowMessage()
+        {
+            mainWindow = new MainWindow();
+            return ($"Text Box: {TextBox}\n");
+        }
+        public bool CanDisplay(object obj)
+        {
+            return true;
+        }
+        public void Display(object obj)
+        {
+            ShowMessage();
+        }
+    }
+}
