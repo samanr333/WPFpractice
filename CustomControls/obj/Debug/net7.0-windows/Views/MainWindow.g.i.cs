74d0bf37418a42234d3b0f39f3bcbf4803edf651
saman333 <samanraut.sr@gmail.com>
2023-09-05 13:08:13 +0545

    feat: add transparent tab control item

--- CustomControls/obj/Debug/net7.0-windows/Views/MainWindow.g.i.cs
+++ CustomControls/obj/Debug/net7.0-windows/Views/MainWindow.g.i.cs
@@ -55,7 +55,7 @@ namespace CustomControls {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CustomControls;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CustomControls;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
---
+++ UserInterface/Controls/HomeTab.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UserInterface.Controls
+{
+    public partial class HomeTab : UserControl
+    {
+        public ObservableCollection<Task> TaskItems { get; set; } = new ObservableCollection<Task>();
+        public HomeTab()
+        {
+            InitializeComponent();
+            DataContext = this;
+            TaskItems.Add(new Task { Title = "Eat", Description = "Eat your breakfast" });
+        }
+        private void AddClick(object sender, RoutedEventArgs e)
+        {
+            string title = TitleBox.Text;
+            string description = DescBox.Text;
+            if (!string.IsNullOrWhiteSpace(title) && !string.IsNullOrWhiteSpace(description))
+            {
+                TaskItems.Add(new Task { Title = title, Description = description });
+            }
+
+        }
+
+        private void DeleteClick(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null && button.DataContext is Task taskItem)
+            {
+                TaskItems.Remove(taskItem);
+            }
+        }
+    }
+}
